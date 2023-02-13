76d1bf47f8b21cbbde7bf9855186f891f580df47
guest271314 <guest271314@gmail.com>
2023-02-12 17:11:55 -0800

    Delete tools directory

--- tools/release/deps.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-
-export * from "https://raw.githubusercontent.com/denoland/automation/0.16.0/mod.ts";
-export * from "https://raw.githubusercontent.com/denoland/automation/0.16.0/github_actions.ts";
